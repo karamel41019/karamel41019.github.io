1963cb0dfd65bea9ccfeb48e280ed3d0f1fa4c41
karamel41019 <59874822+karamel41019@users.noreply.github.com>
2020-01-28 15:37:55 +0200

    Add files via upload

---
+++ many_noties/js.js
@@ -0,0 +1,6 @@
+let factor		= prompt('Введите, пожалуйста, число от 1 до 100', 1); 
+for(let i=1; i<=10; i++)
+{
+	document.write(i + ' * ' + factor + ' = ' + i*factor);
+	document.write('<br>');
+}													
