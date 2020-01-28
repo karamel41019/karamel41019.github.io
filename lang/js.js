6b0dc6465dfb7f1b6feadc4d4fb890ded8ba1096
karamel41019 <59874822+karamel41019@users.noreply.github.com>
2020-01-28 15:43:40 +0200

    Add files via upload

---
+++ lang/js.js
@@ -0,0 +1,12 @@
+function run_system()
+{
+	for(let i=1; i<=10; i++)
+	{
+		for(let j=1; j<=10; j++)
+		{
+			document.write(i + ' * ' + j + ' = ' + i*j + '____');
+			
+		}
+		document.write('<br>');
+	}
+}
